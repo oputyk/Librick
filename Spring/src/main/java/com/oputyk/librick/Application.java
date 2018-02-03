8a2913e0bb679da1778c1667602862a4c7951fa8
Kamil Cegliński <kamilp06@gmail.com>
2018-02-03 11:22:06 +0100

    Spring project initialized.

---
+++ Spring/src/main/java/com/oputyk/librick/Application.java
@@ -0,0 +1,15 @@
+package com.oputyk.librick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by kamil on 02/02/2018.
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
