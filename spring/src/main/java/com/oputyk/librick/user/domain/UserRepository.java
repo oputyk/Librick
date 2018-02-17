0a3325f1aa5fe8e510136dec1c9c21e3099ec5ca
Kamil Cegliński <kamilp06@gmail.com>
2018-02-17 14:35:10 +0100

    UserRepository id changed (id is of type String).

--- spring/src/main/java/com/oputyk/librick/user/domain/UserRepository.java
+++ spring/src/main/java/com/oputyk/librick/user/domain/UserRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 
 
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, String> {
     UserEntity findUserEntityByEmail(String email);
 }
