15caf4113c986fd11e527d219412b96ac5abc597
Kamil Cegliński <kamilp06@gmail.com>
2018-02-17 14:34:04 +0100

    User id changed (id is an email).

--- spring/src/main/java/com/oputyk/librick/user/domain/UserEntity.java
+++ spring/src/main/java/com/oputyk/librick/user/domain/UserEntity.java
@@ -20,11 +20,7 @@ import java.util.List;
 @Table(name = "user")
 public class UserEntity {
     @Id
-    @GeneratedValue
-    private Long id;
-
     private String email;
-
     private String password;
 
     @ElementCollection(targetClass = Role.class)
